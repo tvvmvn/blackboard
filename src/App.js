cce95229ce9363d8163da57016530e2b98852916
tvvmvn <tvvmvn@gmail.com>
2024-02-25 18:30:49 +0900

    2/25

--- src/App.js
+++ src/App.js
@@ -20,10 +20,10 @@ import { useState, useEffect, useRef } from "react";
 */
 
 const DATA = {
-  name: "Galaxy S24",
-  color: ["Black", "Grey", "Purple"],
-  storage: [128, 256, 512],
+  productId: "g0",
+  name: "Galaxy S24 512GB Onyx Black",
   photos: ["Simba.webp", "Timon.webp", "Pumbaa.webp"],
+  price: 999,
   catalog: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
   Facere ad odit assumenda quia perspiciatis corporis eaque itaque nulla eligendi, 
   debitis animi, magni fuga! Voluptas amet doloribus, 
@@ -48,169 +48,179 @@ const DATA = {
 
 export default function App() {
 
-  const [selected, setSelected] = useState({
-    color: DATA.color[0],
-    storage: DATA.storage[0],
-  });
-
+  const [added, addToCart] = useState(localStorage.getItem("cart"));  
+  const [active, setActive] = useState(false);
   const [acc, setAcc] = useState({
     features: false,
     details: false,
     measurements: false
   })
 
-  function handleChange(e) {
-    const name = e.target.name;
-    const value = e.target.value;
-
-    setSelected({ ...selected, [name]: value});
-  }
-
-  function addToCart() {
-    console.log(selected);
-
-    localStorage.setItem("cart", JSON.stringify(selected));
-  }
-
   function toTop() {
     window.document.documentElement.scrollTop = 0;
   }
 
+  // synchronize localStorage
+  useEffect(() => {
+    if (added) {
+      localStorage.setItem("cart", DATA.productId);
+    }
+  }, [added])
+
+  useEffect(() => {
+    document.addEventListener("scroll", () => {
+      const scrollTop = document.documentElement.scrollTop;
+      
+      console.log("scroll top:", scrollTop);
+
+      if (scrollTop > 1000) {
+        setActive(true);
+      } else {
+        setActive(false);
+      }
+    })
+  }, [])
+
   return (
-    <>
-      <h1>Amazon</h1>
-      <p>Appliance / Electric devices / Smartphones</p>
-      <hr />
+    <div id="app">
+      <header className="border-b fixed top-0 left-0 z-10 w-full bg-white">
+        <div className="flex justify-center items-center h-12">
+          <h1 className="text-xl">Amazon</h1>
+        </div>
+      </header>
 
-      <h3>Product Name</h3>
-      <p>{DATA.name}</p>
+      <main className="mt-16 max-w-sm mx-auto">
+        <h3 className="my-4 font-semibold">{DATA.name}</h3>
 
-      <h3>Photos</h3>
-      <Gallery />
+        <Gallery />
 
-      <h3>Colors</h3>
-      <ul>
-        {DATA.color.map(item => (
-          <label key={item}>
-            <input 
-              type="radio" 
-              name="color" 
-              value={item} 
-              checked={item == selected.color}
-              onChange={handleChange} 
-            />
-            {item} {" "}
-          </label> 
-        ))}
-      </ul>
+        <div className="text-4xl font-semibold my-4">
+          ${DATA.price}
+        </div>
 
-      <h3>Storage</h3>
-      <ul>
-        {DATA.storage.map(item => (
-          <label key={item}>
-            <input 
-              type="radio" 
-              name="storage" 
-              value={item} 
-              checked={item == selected.storage}
-              onChange={handleChange} 
-            />
-            {item}GB {" "}
-          </label>
-        ))}
-      </ul>
+        <button 
+          type="button" 
+          className="w-full p-2 bg-yellow-400 rounded-full disabled:opacity-50"
+          onClick={() => addToCart(DATA.productId)}
+          disabled={added}
+        >
+          Add To Cart
+        </button>
 
-      <button type="button" onClick={addToCart}>
-        Add To Cart
-      </button>
+        <h3 className="my-4 font-semibold">
+          From the manufacturer
+        </h3>
+        <h1 className="text-6xl text-gray-200">{DATA.catalog}</h1>
 
-      <hr />
+        <h3 className="my-4 font-semibold">
+          Product information
+        </h3>
 
-      <h3>Product Details</h3>
-      <h1 style={{ color: "#ddd" }}>{DATA.catalog}</h1>
-
-      <hr />
-
-      <h3>Product information (Accordion)</h3>
-
-      <div className="">
-        <section>
-          <button 
-            onClick={() => setAcc({ ...acc, features: !acc.features})}
+        <ul className="">
+          <li className="mb-1">
+            <button
+              className="w-full p-2 bg-gray-200 text-left"
+              onClick={() => setAcc({ ...acc, features: !acc.features })}
             >
               Feature & Specs
             </button>
-          <div 
-            className="hidden"
-            style={{ display: acc.features && "block" }}
-          >
-            <p>OS: {DATA.info.features.OS}</p>
-            <p>Celluar Technology: {DATA.info.features.Celluar}</p>
-            <p>Connector Type: {DATA.info.features.Connector}</p>
-          </div>
-        </section>
-        <section>
-          <button 
-            onClick={() => setAcc({ ...acc, details: !acc.details})}
+            <div
+              className="hidden"
+              style={{ display: acc.features && "block" }}
+            >
+              <p>OS: {DATA.info.features.OS}</p>
+              <p>Celluar Technology: {DATA.info.features.Celluar}</p>
+              <p>Connector Type: {DATA.info.features.Connector}</p>
+            </div>
+          </li>
+          <li className="mb-1">
+            <button
+              className="w-full p-2 bg-gray-200 text-left"
+              onClick={() => setAcc({ ...acc, details: !acc.details })}
             >
               Item Details
             </button>
-          <div 
-            className="hidden"
-            style={{ display: acc.details && "block" }}
-          >
-            <p>Customer Reviews: {DATA.info.details.reviews}</p>
-            <p>Brand: {DATA.info.details.brand}</p>
-            <p>Year: {DATA.info.details.year}</p>
-          </div>
-        </section>
-        <section>
-          <button 
-            onClick={() => setAcc({ ...acc, measurements: !acc.measurements})}
+            <div
+              className="hidden"
+              style={{ display: acc.details && "block" }}
+            >
+              <p>Customer Reviews: {DATA.info.details.reviews}</p>
+              <p>Brand: {DATA.info.details.brand}</p>
+              <p>Year: {DATA.info.details.year}</p>
+            </div>
+          </li>
+          <li className="mb-1">
+            <button
+              className="w-full p-2 bg-gray-200 text-left"
+              onClick={() => setAcc({ ...acc, measurements: !acc.measurements })}
             >
               Measurements
             </button>
-          <div 
-            className="hidden"
-            style={{ display: acc.measurements && "block" }}
-          >
-            <p>Weight: {DATA.info.measurements.weight} grams</p>
-            <p>Item Dimensions LxWxH: {DATA.info.measurements.dimension} inch</p>
-          </div>
-        </section>
-      </div>
+            <div
+              className="hidden"
+              style={{ display: acc.measurements && "block" }}
+            >
+              <p>Weight: {DATA.info.measurements.weight} grams</p>
+              <p>Item Dimensions LxWxH: {DATA.info.measurements.dimension} inch</p>
+            </div>
+          </li>
+        </ul>
 
-      <hr />
-
-      <button onClick={toTop}>Bring me TOP</button>
-    </>  
+        <button 
+          className="fixed right-4 bottom-4 bg-gray-200 p-2"
+          onClick={toTop}
+          style={{ display: active ? "block" : "none" }}
+        >
+          TOP
+        </button>
+      </main>
+      <footer className="p-8 bg-gray-400 mt-12 text-center">
+        2024 Amazon
+      </footer>
+    </div>  
   )
 }
 
 function Gallery() {
   const [index, setIndex] = useState(0);
 
+  const prevBtn = (
+    <button 
+      className="absolute top-0 left-0 h-full px-2 text-4xl text-white"
+      onClick={() => setIndex(index - 1)}
+    >
+      &#10094;
+    </button>
+  )
+
+  const nextBtn = (
+    <button 
+      className="absolute top-0 right-0 h-full px-2 text-4xl text-white"
+      onClick={() => setIndex(index + 1)}
+    >
+      &#10095;
+    </button>  
+  )
+
   return (
     <>
-      <img 
-        src={process.env.PUBLIC_URL + "/images/" + DATA.photos[index]} 
-        style={{ width: "300px", height: "300px", objectFit: "cover" }}
-        alt=""
-      />
+      <div className="mx-auto w-[300px] h-[300px] relative border">
+        <img 
+          className="block w-full h-full object-cover"
+          src={process.env.PUBLIC_URL + "/images/" + DATA.photos[index]} 
+          alt=""
+        />
+        {(index != 0) && prevBtn}
+        {(index != 2) && nextBtn}
+      </div>
 
-      <p>
-        <button onClick={() => setIndex(index - 1)}>Prev</button>
-        <button onClick={() => setIndex(index + 1)}>Next</button>
-      </p>
 
-      <div className="">
+      <div className="flex justify-center">
         {DATA.photos.map((photo, i) => (
           <span
             key={i}
+            className=""
             style={{ 
-              width: "100px", 
-              height: "100px", 
-              objectFit: "cover",
               color: index == i ? "red" : "black"
             }}
           >
