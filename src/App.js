a6da3d36bc28a9a8d67f7ec3b4392ddf0421dd45
tvvmvn <tvvmvn@gmail.com>
2024-02-25 16:10:54 +0900

    2/25

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 
 /*
-  # Amazon product details page
+  # Amazon DATA details page
 
   1 Product photos
   Gallery
@@ -12,24 +12,45 @@ import { useState, useEffect, useRef } from "react";
   3 Product details
   Accordion
 
-  5 Reviews
+  5 Reviews (-)
   List rendering
 
   6 Top button
   scroll event
 */
 
-const product = {
+const DATA = {
   name: "Galaxy S24",
   color: ["Black", "Grey", "Purple"],
-  storage: [128, 256, 512]
+  storage: [128, 256, 512],
+  photos: ["Simba.webp", "Timon.webp", "Pumbaa.webp"],
+  catalog: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+  Facere ad odit assumenda quia perspiciatis corporis eaque itaque nulla eligendi, 
+  debitis animi, magni fuga! Voluptas amet doloribus, 
+  sapiente nihil rerum tempora?`,
+  info: {
+    features: {
+      OS: "Android 14",
+      Celluar: "5G Connector",
+      Connector: "USB Type C"
+    },
+    details: {
+      reviews: "3.9 out of 5",
+      brand: "SAMSUNG",
+      year: 2024
+    },
+    measurements: {
+      weight: 197,
+      dimension: "6.24 x 2.99 x 0.3"
+    }
+  }
 }
 
 export default function App() {
 
-  const [cart, setCart] = useState({
-    color: "",
-    storage: "",
+  const [selected, setSelected] = useState({
+    color: DATA.color[0],
+    storage: DATA.storage[0],
   });
 
   const [acc, setAcc] = useState({
@@ -42,11 +63,13 @@ export default function App() {
     const name = e.target.name;
     const value = e.target.value;
 
-    setCart({ ...cart, [name]: value});
+    setSelected({ ...selected, [name]: value});
   }
 
   function addToCart() {
-    console.log(cart);
+    console.log(selected);
+
+    localStorage.setItem("cart", JSON.stringify(selected));
   }
 
   function toTop() {
@@ -60,19 +83,20 @@ export default function App() {
       <hr />
 
       <h3>Product Name</h3>
-      <p>{product.name}</p>
+      <p>{DATA.name}</p>
 
-      <h3>Images</h3>
-      <p>Gallery</p>
+      <h3>Photos</h3>
+      <Gallery />
 
       <h3>Colors</h3>
       <ul>
-        {product.color.map(item => (
+        {DATA.color.map(item => (
           <label key={item}>
             <input 
               type="radio" 
               name="color" 
               value={item} 
+              checked={item == selected.color}
               onChange={handleChange} 
             />
             {item} {" "}
@@ -82,12 +106,13 @@ export default function App() {
 
       <h3>Storage</h3>
       <ul>
-        {product.storage.map(item => (
+        {DATA.storage.map(item => (
           <label key={item}>
             <input 
               type="radio" 
               name="storage" 
               value={item} 
+              checked={item == selected.storage}
               onChange={handleChange} 
             />
             {item}GB {" "}
@@ -102,13 +127,7 @@ export default function App() {
       <hr />
 
       <h3>Product Details</h3>
-
-      <h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Facere ad odit assumenda quia perspiciatis corporis eaque itaque nulla eligendi, 
-        debitis animi, magni fuga! Voluptas amet doloribus, 
-        sapiente nihil rerum tempora?
-      </h1>
+      <h1 style={{ color: "#ddd" }}>{DATA.catalog}</h1>
 
       <hr />
 
@@ -125,7 +144,9 @@ export default function App() {
             className="hidden"
             style={{ display: acc.features && "block" }}
           >
-            features...
+            <p>OS: {DATA.info.features.OS}</p>
+            <p>Celluar Technology: {DATA.info.features.Celluar}</p>
+            <p>Connector Type: {DATA.info.features.Connector}</p>
           </div>
         </section>
         <section>
@@ -138,21 +159,65 @@ export default function App() {
             className="hidden"
             style={{ display: acc.details && "block" }}
           >
-            details...
+            <p>Customer Reviews: {DATA.info.details.reviews}</p>
+            <p>Brand: {DATA.info.details.brand}</p>
+            <p>Year: {DATA.info.details.year}</p>
+          </div>
+        </section>
+        <section>
+          <button 
+            onClick={() => setAcc({ ...acc, measurements: !acc.measurements})}
+            >
+              Measurements
+            </button>
+          <div 
+            className="hidden"
+            style={{ display: acc.measurements && "block" }}
+          >
+            <p>Weight: {DATA.info.measurements.weight} grams</p>
+            <p>Item Dimensions LxWxH: {DATA.info.measurements.dimension} inch</p>
           </div>
         </section>
       </div>
 
       <hr />
 
-      <h3>Customer reviews</h3>
-      <ul>
-        <li>..</li>
-        <li>..</li>
-        <li>..</li>
-      </ul>
-
       <button onClick={toTop}>Bring me TOP</button>
+    </>  
+  )
+}
+
+function Gallery() {
+  const [index, setIndex] = useState(0);
+
+  return (
+    <>
+      <img 
+        src={process.env.PUBLIC_URL + "/images/" + DATA.photos[index]} 
+        style={{ width: "300px", height: "300px", objectFit: "cover" }}
+        alt=""
+      />
+
+      <p>
+        <button onClick={() => setIndex(index - 1)}>Prev</button>
+        <button onClick={() => setIndex(index + 1)}>Next</button>
+      </p>
+
+      <div className="">
+        {DATA.photos.map((photo, i) => (
+          <span
+            key={i}
+            style={{ 
+              width: "100px", 
+              height: "100px", 
+              objectFit: "cover",
+              color: index == i ? "red" : "black"
+            }}
+          >
+            *
+          </span>
+        ))}
+      </div>
     </>  
   )
 }
